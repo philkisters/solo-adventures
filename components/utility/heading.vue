ac08fed3e465479d03a7c9069f0368029e9c00ec
Philipp Kisters <kisters@informatik.uni-hamburg.de>
2021-09-26 15:45:28 +0200

    initial commit

---
+++ components/utility/heading.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="">
+    <slot />
+  </div>
+</template>
+<script>
+export default {
+  props: {
+    size: {
+      typeof: String,
+      default: 'md'
+    }
+  }
+}
+</script>
